d6620642e83d621cf365788e57c99fdcf9c4bf92
Aditya <mauryaaditya02003@gmail.com>
2026-07-24 19:39:15 +0530

    first commit

---
+++ test.py
@@ -0,0 +1,8 @@
+n = int(input("Enter a number: "))
+
+if n == 1:
+    m = int(input("enter how many times : "))
+    for i in range(m):  
+        print(5* " hello ")
+else:
+    print("Exiting....")
