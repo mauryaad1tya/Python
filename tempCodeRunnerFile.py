692889f0b4a40cbb35299453dbffe03be4e231a2
Aditya <mauryaaditya02003@gmail.com>
2026-07-25 23:46:35 +0530

    practice

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+
+# for i in range (2, 21, 2):
+#     print(i)
+
+s
--- test.py
+++ test.py
@@ -1,8 +1,172 @@
-n = int(input("Enter a number: "))
+# n = int(input("Enter a number: "))
 
-if n == 1:
-    m = int(input("enter how many times : "))
-    for i in range(m):  
-        print(5* " hello ")
-else:
-    print("Exiting....")
+# if n == 1:
+#     m = int(input("enter how many times : "))
+#     for i in range(m):  
+#         print(5* " hello ")
+# else:
+#     print("Exiting....")
+
+
+# print("hello world")
+
+# print(90)
+
+# name = "aditya"
+# print(name)
+
+# score = 10
+# print(score)
+# score = 50
+# print(score)
+
+
+# print("Welcome to this program")
+# print(89+90)
+# print(90)
+# age = 23
+# print(age)
+
+# price = 230
+# print(price)
+# price = 239
+# print(price)
+
+# name = input("Enter your name :")
+# age = input("Enter your age :")
+# city = input("Enter your city :")
+
+
+# print("Name :",name)
+# print("age :",age)
+# print("City :",city)
+
+# age = int(input("Enter age: "))
+# print(age + age)
+
+# print(type(age))
+
+# print(10<5)
+
+# if (10>50):
+#     print("yes")
+# else:
+#     print("No")
+
+# age = int(input("Enter your age: "))
+
+# if age >= 18:
+#     print("You are eligible to vote.")
+
+# passw = input("Enter the password")
+
+# if passw == "python123":
+#     print("Login Successful")
+# else:
+#     print("Wrong Pass")
+
+# marks = int(input("Enter the marks :"))
+
+# if marks >= 95 :
+#     print("Grade A")
+# elif marks >=82 :
+#     print("Grade B")
+# elif marks >=65:
+#     print("Grade C")
+# elif marks >=40:
+#     print("Grade D")
+# else :
+#     print("fail")
+
+
+
+# number = int(input("Enter: "))
+
+# if number == 0:
+#     print("Zero")
+
+# elif number >0:
+#     print("Positive")
+# else :
+#     print("Negative")
+
+
+# balance = int(input("enter:"))
+
+# if balance >=5000:
+#     print("Premium account")
+# elif balance >=1000:
+#     print("Standard account")
+# else:
+#     print("Low balance")
+
+# username = input("Enter username: ")
+# password = input("Enter password: ")
+
+# if username == "admin" & password == "python123":
+#     print("Login successful")
+
+# elif username != "admin":
+#     print("Wrong username")
+# elif password != "python123":
+#     print("Wrong password")
+# else:
+#     print("wrong username and password")
+
+
+
+# value = int(input("Enter: "))
+# count = 1
+# while count <= value:
+#     print(count)
+#     count = count + 1
+    
+# for i in range(1, 11):
+#     print(i)
+
+# for i in range(10, 0, -1):
+#     print(i)
+
+# for i in range (2, 21, 2):
+#     print(i)
+
+# for i in range(1,11):
+#     print("5 x",i ,"=",i*5)
+
+# num = int(input("Enter a number: "))
+# for i in range(1,11):
+#     print(f"{num} x {i} = {i *num}")
+
+# hello = int(input("Enter: "))
+
+# for i in range(hello):
+#     print("Hello")
+
+# squ = int(input("enter the number: "))
+# for i in range(1,11):
+#     print(i*i)
+
+# num = int(input("Enter: "))
+# for i in range(1, num+1):
+#     print(i)
+
+
+
+# for i in range(1,num+1):
+#     if (i %2  == 0):
+#         print(i)
+        
+#         num = int(input("Enter: "))
+# num = int(input("enter:"))
+# for i in range(2, num + 1, 2):
+#     print(i)
+
+
+# num = int(input("enter :"))
+
+# for i in range(1,num+1):
+#     print("*"* i)
+for i in range(1, 6):
+    for j in range(i):
+        print("*", end="")
+    print()
